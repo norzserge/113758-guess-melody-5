740feae5ec350d3445159b4e03910bcb572b4c69
norzserge <metelskiisa@mail.ru>
2020-09-30 08:31:28 +0300

    Added App and Welcome components

---
+++ src/components/welcome-screen/welcome-screen.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const WelcomeScreen = (props) => {
+  const {errorsCount} = props;
+  return (<h1>{errorsCount}</h1>);
+};
+
+WelcomeScreen.propTypes = {
+  errorsCount: PropTypes.number.isRequired,
+};
+
+export default WelcomeScreen;
