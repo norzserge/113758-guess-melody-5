a33e2c92e146f4857800deb3930a65d6cc69d4b4
norzserge <metelskiisa@mail.ru>
2020-10-02 00:17:17 +0300

    routing added

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,10 +1,39 @@
 import React from "react";
 import PropTypes from "prop-types";
+import {BrowserRouter, Switch, Route} from "react-router-dom";
 import WelcomeScreen from "../welcome-screen/welcome-screen";
+import SignIn from "../login/login";
+import GameOver from "../lose/lose";
+import Result from "../result/result";
+import Artist from "../artist/artist";
+import Genre from "../genre/genre";
 
 const App = (props) => {
   const {errorsCount} = props;
-  return <WelcomeScreen errorsCount={errorsCount} />;
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/">
+          <WelcomeScreen errorsCount={errorsCount} />
+        </Route>
+        <Route exact path="/login">
+          <SignIn />
+        </Route>
+        <Route exact path="/result">
+          <Result />
+        </Route>
+        <Route exact path="/lose">
+          <GameOver />
+        </Route>
+        <Route exact path="/dev-artist">
+          <Artist />
+        </Route>
+        <Route exact path="/dev-genre">
+          <Genre />
+        </Route>
+      </Switch>
+    </BrowserRouter>
+  );
 };
 
 App.propTypes = {
